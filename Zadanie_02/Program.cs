f09cf8cd3d51f037235c64a9ba6f067d91278344
Eduard <eduard.sher1@gmail.com>
2023-01-16 11:02:47 +0300

    Added chek of he number B to the number A per square

---
+++ Zadanie_02/Program.cs
@@ -0,0 +1,10 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if(numberA == numberB * numberB)
+Console.WriteLine("Yes! Первое число, является квадратом второго числа.");
+if(numberA != numberB * numberB)
+Console.WriteLine("No! Первое число, не является квадратом второго числа.");
