4f5b73cb260317d558621c09e3caccaec609883a
Eduard <eduard.sher1@gmail.com>
2023-01-18 02:42:36 +0300

    Zadanie_03 Added поиск названия дня недели по номеру

--- Zadanie_03/Program.cs
+++ Zadanie_03/Program.cs
@@ -1,28 +1,12 @@
 ﻿// По заданному номеру дня недели вывести его название
 
 Console.Write("Введите день недели от 1 до 7: ");
-string username = Console.ReadLine();
+string day = Console.ReadLine();
 
-if(username == "1")
-{
-    Console.WriteLine("Понедельник");
-}
-
-if(username == "2")
-{
-    Console.WriteLine("Вторник");
-}
-if(username == "3");
-{
-    Console.WriteLine("Среда");
-}
-
-if(username == "4");
-{
-    Console.WriteLine("Четверг");
-}
-
-if(username == "5");
-{
-    Console.WriteLine("Пятница");
-}
+if (day == "1") Console.WriteLine("Понедельник");
+if (day == "2") Console.WriteLine("Вторник");
+if (day == "3") Console.WriteLine("Среда");
+if (day == "4") Console.WriteLine("Четверг");
+if (day == "5") Console.WriteLine("Пятница");
+if (day == "6") Console.WriteLine("Суббота");
+if (day == "7") Console.WriteLine("Воскресенье");
