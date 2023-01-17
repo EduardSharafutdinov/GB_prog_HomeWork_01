1a1b6ac6ab9e6cc03228c8846d40825a4ee70ff5
Eduard <eduard.sher1@gmail.com>
2023-01-17 13:39:45 +0300

    Zadanie_05 Added parity chek

---
+++ Zadanie_05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число для проверки: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numberA);
+
+if (numberA%2 != 0)
+{
+    Console.Write("Данное число является не четным");
+}
+else
+{
+    Console.Write("Данное число является четным");
+}
