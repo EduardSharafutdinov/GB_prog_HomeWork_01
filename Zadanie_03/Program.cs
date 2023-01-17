f3ca745cb871bf53a8615fcfc0739aedbcfd9c1e
Eduard <eduard.sher1@gmail.com>
2023-01-17 12:40:13 +0300

    Zadanie_03 added search forthe day of the week

---
+++ Zadanie_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите день недели от 1 до 7: ");
+string username = Console.ReadLine();
+
+if(username == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+
+if(username == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if(username == "3");
+{
+    Console.WriteLine("Среда");
+}
+
+if(username == "4");
+{
+    Console.WriteLine("Четверг");
+}
+
+if(username == "5");
+{
+    Console.WriteLine("Пятница");
+}
---
+++ Zadanie_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти максимальное из трех чисел
+
+int numberA = new Random().Next(1, 1000);      // 1 2 3 4 .... 1000
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 1000);      // 1 2 3 4 .... 1000
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 1000);      // 1 2 3 4 .... 1000
+Console.WriteLine(numberC);
+
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
