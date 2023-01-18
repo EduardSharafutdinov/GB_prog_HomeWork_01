1f5c563fcd5bd276011b54d582b0afb85cb66e34
Eduard <eduard.sher1@gmail.com>
2023-01-18 20:17:34 +0300

    Zadanie_08 Показать вторую цифру трёхзначного числа

---
+++ Zadanie_08/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите любое трехзначное число в диапазоне от 100 до 999: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = (numberA % 100 - numberA % 10) / 10;
+
+Console.WriteLine(numberB);
