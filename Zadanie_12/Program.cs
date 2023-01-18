d28e86f90fac05e87517125afc0dfb280573e597
Eduard <eduard.sher1@gmail.com>
2023-01-19 01:14:23 +0300

    Zadanie_12 Найти третью цифру числа или сообщить, что её нет

---
+++ Zadanie_12/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите любое трехзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA <=99) Console.WriteLine("Вы ввели число где нет третьей цифры. Повторите ввод.");
+
+if (numberA >99)
+{
+int numberB = numberA % 10;
+Console.Write("Третья цифра введеного Вами числа = ");
+Console.WriteLine(numberB);
+}
