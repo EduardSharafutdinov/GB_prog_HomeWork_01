d20e210290547df1e4105b89133e165999c3e493
Eduard <eduard.sher1@gmail.com>
2023-01-18 20:06:59 +0300

    Zadanie_07 Показать последнюю цифру трехзначного числа

---
+++ Zadanie_07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите любое трехзначное число от 100 до 999: ");
+int numberA = Convert.ToInt16(Console.ReadLine());
+
+int numberB = numberA % 10;
+Console.Write("Последняя цифра Вашего числа = ");
+Console.WriteLine(numberB);
+
